529d8b64c839a4bee8c159e5ff920153c982705e
vargasnolivos <hernan.vargas@utm.edu.ec>
2025-05-26 11:31:55 -0500

    compilado correctamente

---
+++ principal.py
@@ -0,0 +1,8 @@
+from listas import ListasPOO
+tam=int(input("ingrese un tamaño de la lista "))
+List=[]
+c=ListasPOO()
+c.ingreso(List,tam)
+c.imprimir(List)
+print(c.factorial(tam))
+print(c.fibonacci(tam))
